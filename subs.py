566bdb2a4fa7f25d48a49244d297b6b182ac2688
215355 <84146574+Zisess@users.noreply.github.com>
2023-02-04 17:49:47 +0300

    Update subs.py

--- subs.py
+++ subs.py
@@ -1,4 +1,4 @@
-def stritm(xstr,ystr,zstr):
+def find_and_strip_substrings_1(xstr,ystr,zstr):
     results = []
     for i in range(len(xstr)):
         if(ystr in xstr[i:i+len(ystr)]):
@@ -14,3 +14,34 @@ def stritm(xstr,ystr,zstr):
                         results.append(fstr)
                         break
     return results
+
+def find_and_strip_substrings_2(xstr, ystr, zstr):
+    result = ""
+    i = 0
+    while i < len(xstr):
+        y_start = xstr.find(ystr, i)
+        if y_start == -1:
+            break
+        z_start = xstr.find(zstr, y_start + len(ystr))
+        if z_start == -1:
+            break
+        fstr = xstr[y_start:z_start + len(zstr)]
+        if fstr not in result:
+            result += (fstr)
+        i = z_start + len(zstr)
+    return result.replace(ystr,"").replace(zstr,"")
+
+def find_and_strip_substrings_3(xstr, ystr, zstr):
+    result = []
+    i = 0
+    while i < len(xstr):
+        y_start = xstr.find(ystr, i)
+        if y_start == -1:
+            break
+        z_start = xstr.find(zstr, y_start + len(ystr))
+        if z_start == -1:
+            break
+        fstr = xstr[y_start:z_start].replace(ystr, "").replace(zstr, "")
+        result.append(fstr)
+        i = z_start + len(zstr)
+    return "".join(result)
